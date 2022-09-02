1a26c59b375eb7a57ec1ac8f3309cf8b925c046f
developerlenin <justlenin9095@gmail.com>
2022-09-02 12:05:42 +0530

    new

---
+++ operators.js
@@ -0,0 +1,22 @@
+{
+	let number = 5;
+let number1 =10;
+document.write(number + number1)
+document.write("<br>");
+}
+{
+	var number2 = 10;
+	var number3= 5;
+	document.write(number2 - number3)
+	document.write("<br>");
+}
+{
+	const number4 = 5;
+	const number5 = 5;
+	document.write(number4*number5)
+	document.write("<br>");
+}
+{
+	let number4 = 5;
+}
+  
